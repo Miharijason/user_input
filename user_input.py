238e536b7b8afdc34492bcd556c14f2734dfd656
Miharijason <jasonmihari0@gmail.com>
2024-02-29 18:41:32 +0300

    initial python code

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name =input("What is your name?")
+age =input("How old are you?")
+location=input("Where are you located ?")
+print=("Welcome here!")
+print=("Hello"+name+"you are"+age+"years old and live in"+location)
